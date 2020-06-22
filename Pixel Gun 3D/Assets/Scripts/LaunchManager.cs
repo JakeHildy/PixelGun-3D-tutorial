e33e23830f2c821b30a8c32ccf68b858c87ff649
jacobHildebrandt <jake@hildy.ca>
2020-06-21 21:52:20 -0700

    Added Photon and setup basic Photon Connection

---
+++ Pixel Gun 3D/Assets/Scripts/LaunchManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class LaunchManager : MonoBehaviourPunCallbacks
+{
+    public GameObject EnterGamePanel;
+    public GameObject ConnectionStatusPanel;
+    public GameObject LobbyPanel;
+
+    #region Unity Methods
+    // Start is called before the first frame update
+    void Start()
+    {
+        EnterGamePanel.SetActive(true);
+        ConnectionStatusPanel.SetActive(false);
+        LobbyPanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    #endregion
+
+
+
+    // =======================================
+    #region Public Methods
+
+    public void ConnectToPhotonServer()
+    {
+        if(!PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.ConnectUsingSettings();
+            EnterGamePanel.SetActive(false);
+            ConnectionStatusPanel.SetActive(true);
+        }
+    }
+
+    public void JoinRandomRoom()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    #endregion
+
+    // =======================================
+    #region Photon Callbacks
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log(PhotonNetwork.NickName + " Connected to Photon server.");
+        LobbyPanel.SetActive(true);
+        ConnectionStatusPanel.SetActive(false);
+    }
+
+    public override void OnConnected()
+    {
+        Debug.Log("Connected to Internet.");
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        base.OnJoinRandomFailed(returnCode, message);
+        Debug.Log(message);
+    }
+
+    #endregion
+}
