7f48f3fb5520db0f7f7efdd777757bc0e47b5d4d
jacobHildebrandt <jake@hildy.ca>
2020-06-22 08:11:19 -0700

    Added Basic Game Scene
    Two players can now join the same game room. No player movement yet.

--- Pixel Gun 3D/Assets/Scripts/LaunchManager.cs
+++ Pixel Gun 3D/Assets/Scripts/LaunchManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
 
 public class LaunchManager : MonoBehaviourPunCallbacks
 {
@@ -10,6 +11,12 @@ public class LaunchManager : MonoBehaviourPunCallbacks
     public GameObject LobbyPanel;
 
     #region Unity Methods
+
+    private void Awake() 
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -65,6 +72,34 @@ public class LaunchManager : MonoBehaviourPunCallbacks
     {
         base.OnJoinRandomFailed(returnCode, message);
         Debug.Log(message);
+        CreateAndJoinRoom();
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log(PhotonNetwork.NickName + " joined to " + PhotonNetwork.CurrentRoom.Name);
+        PhotonNetwork.LoadLevel("GameScene");
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Debug.Log(newPlayer.NickName + " joined to " + PhotonNetwork.CurrentRoom.Name + " " + PhotonNetwork.CurrentRoom.PlayerCount);
+    }
+
+    #endregion
+
+
+    // =======================================
+    #region Private Methods
+    private void CreateAndJoinRoom()
+    {
+        string randomRoomName = "Room " + Random.Range(0,10000);
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.IsOpen = true;
+        roomOptions.IsVisible = true;
+        roomOptions.MaxPlayers = 20;
+
+        PhotonNetwork.CreateRoom(randomRoomName, roomOptions);
     }
 
     #endregion
