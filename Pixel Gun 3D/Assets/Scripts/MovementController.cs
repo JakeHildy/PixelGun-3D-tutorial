55374987e39081f28a461b76e92d6eaade88e2bd
jacobHildebrandt <jake@hildy.ca>
2020-06-22 20:44:28 -0700

    Added Mouse FPS Movement

--- Pixel Gun 3D/Assets/Scripts/MovementController.cs
+++ Pixel Gun 3D/Assets/Scripts/MovementController.cs
@@ -1,12 +1,20 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MovementController : MonoBehaviour
 {
     [SerializeField] float speed = 5.0f;
+    [SerializeField] float lookSensitivity = 3.0f;
+
+    [SerializeField] GameObject fpsCamera;
         
     private Vector3 velocity = Vector3.zero;
+    private Vector3 rotation = Vector3.zero;
+    private float cameraUpAndDownRotation = 0f;
+    private float currentCameraUpAndDownRotation = 0f;
+
     Rigidbody rb;
 
     // Start is called before the first frame update
@@ -30,7 +38,24 @@ public class MovementController : MonoBehaviour
 
         // Apply movement
         Move(_movementVelocity);
+
+
+        // calculate rotation as a 3DD vector for turning around.
+        float _yRotation = Input.GetAxis("Mouse X");
+        Vector3 _rotationVector = new Vector3(0, _yRotation) * lookSensitivity;
+
+        // Apply rotation
+        Rotate(_rotationVector);
+
+        // Calculate look up and down camera rotation
+        float _cameraUpDownRotation = Input.GetAxis("Mouse Y") * lookSensitivity;
+
+        // Apply rotation
+        RotateCamera(_cameraUpDownRotation);
+
     }
+
+
 
     private void FixedUpdate() 
     {
@@ -38,12 +63,33 @@ public class MovementController : MonoBehaviour
         {
             rb.MovePosition(rb.position + velocity*Time.fixedDeltaTime);
         }
+
+        rb.MoveRotation(rb.rotation*Quaternion.Euler(rotation));
+
+        if (fpsCamera != null)
+        {
+            currentCameraUpAndDownRotation -= cameraUpAndDownRotation;
+            currentCameraUpAndDownRotation = Mathf.Clamp(currentCameraUpAndDownRotation, -85, 85);
+
+            fpsCamera.transform.localEulerAngles = new Vector3(currentCameraUpAndDownRotation, 0, 0);
+        }
     }
 
 
     private void Move(Vector3 movementVelocity)
     {
         velocity = movementVelocity;
-
     }
+
+    private void Rotate(Vector3 rotationVector)
+    {
+        rotation = rotationVector;
+    }
+
+    private void RotateCamera(float cameraUpAndDownRot)
+    {
+        cameraUpAndDownRotation = cameraUpAndDownRot;
+    }
+
+
 }
