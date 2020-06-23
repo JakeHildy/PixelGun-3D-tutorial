e982092602a4ffd17cb0164c32d8b5cf5d55ca86
jacobHildebrandt <jake@hildy.ca>
2020-06-22 21:48:35 -0700

    Added Multiplayer!

--- Pixel Gun 3D/Assets/Scripts/PixelGunGameManager.cs
+++ Pixel Gun 3D/Assets/Scripts/PixelGunGameManager.cs
@@ -6,10 +6,19 @@ using Photon.Realtime;
 
 public class PixelGunGameManager : MonoBehaviourPunCallbacks
 {
+    [SerializeField] GameObject playerPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(PhotonNetwork.IsConnected)
+        {
+            if (playerPrefab != null)
+            {
+                int randomPoint = Random.Range(-10, 10);
+                PhotonNetwork.Instantiate(playerPrefab.name, new Vector3(randomPoint, 0, randomPoint), Quaternion.identity);
+            }            
+        }
     }
 
     // Update is called once per frame
