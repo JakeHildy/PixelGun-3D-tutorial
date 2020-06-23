a35d4e020b9c5672db83a04c229ddaa769dfc952
jacobHildebrandt <jake@hildy.ca>
2020-06-22 22:52:32 -0700

    Added Taking Damage RPC

--- Pixel Gun 3D/Assets/Scripts/Shooting.cs
+++ Pixel Gun 3D/Assets/Scripts/Shooting.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class Shooting : MonoBehaviour
 {
@@ -36,6 +37,11 @@ public class Shooting : MonoBehaviour
             if(Physics.Raycast(ray, out _hit, 100))
             {
                 Debug.Log(_hit.collider.gameObject.name);
+                if (_hit.collider.gameObject.CompareTag("Player") && !_hit.collider.gameObject.GetComponent<PhotonView>().IsMine)
+                {
+                    _hit.collider.gameObject.GetComponent<PhotonView>().RPC("TakeDamage", RpcTarget.AllBuffered, 10f);
+                }
+                
             }
         }
     }
